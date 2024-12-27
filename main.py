630f668756e39d18b3c9cb13cb8f7af2abeaa584
Abdul Moeed <abdul.moeed@emblemtechnologies.co>
2024-12-28 04:20:32 +0500

    added method to calculate tax

--- main.py
+++ main.py
@@ -35,3 +35,63 @@ def get_salary() -> tuple[int, str]:
         raise ValueError('Invalid input. Enter an integer value for salary.')
 
     return salary, category
+
+
+def convert_amount(amount: int, to_yearly: bool) -> int:
+    """
+    Converts the given amount between monthly and yearly values.
+
+    Args:
+        amount (int): The amount to be converted.
+        to_yearly (bool): If True, converts the amount to yearly. If False, converts the amount to monthly.
+
+    Returns:
+        int: The converted amount, rounded to the nearest integer.
+    """
+    return round(amount * 12 if to_yearly else amount / 12)
+
+
+def calculate_yearly_income_tax(yearly_salary: int) -> int:
+    """
+    Calculates the yearly income tax based on the given yearly salary.
+
+    Args:
+        yearly_salary (int): The yearly salary for which the income tax is to be calculated.
+
+    Returns:
+        int: The calculated yearly income tax, rounded to the nearest integer.
+    """
+    yearly_income_tax, previous_bracket_limit = 0, 0
+
+    for bracket_limit, tax_rate in tax_brackets:
+        if yearly_salary <= bracket_limit:
+            yearly_income_tax = yearly_income_tax + (yearly_salary - previous_bracket_limit) * tax_rate
+            break
+        yearly_income_tax = yearly_income_tax + (bracket_limit - previous_bracket_limit) * tax_rate
+        previous_bracket_limit = bracket_limit
+
+    return round(yearly_income_tax)
+
+
+def main():
+    salary, category = get_salary()
+
+    yearly_salary = convert_amount(salary, to_yearly=(category == 'M'))
+    monthly_salary = convert_amount(yearly_salary, to_yearly=False)
+
+    yearly_income_tax = calculate_yearly_income_tax(yearly_salary)
+    monthly_income_tax = convert_amount(yearly_income_tax, to_yearly=False)
+
+    print(f'{category_dict.get("M")} salary {monthly_salary} PKR')
+    print(f'Monthly income tax {monthly_income_tax} PKR')
+    print(f"Your monthly salary after tax {monthly_salary - monthly_income_tax} PKR")
+
+    print()
+
+    print(f'{category_dict.get("Y")} salary {yearly_salary} PKR')
+    print(f'Yearly income tax {yearly_income_tax} PKR')
+    print(f"Yearly salary after tax {yearly_salary - yearly_income_tax} PKR")
+
+
+if __name__ == '__main__':
+    main()
